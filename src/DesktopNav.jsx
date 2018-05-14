cf011b18db7d66a52e2041f516c15bf079f41dcf
Elbehary <ahmedelbehary0@gmail.com>
2018-05-14 15:44:50 +0200

    pass dirctly the value from App to descendants

--- src/DesktopNav.jsx
+++ src/DesktopNav.jsx
@@ -1,28 +1,51 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { ThemeContext } from './App.jsx';
 
 export default function DesktopNav(props) {
     return (
-        <ul className="right hide-on-med-and-down">
-            <li>
-                <a className={' ' + (props.theme === 'dark' ? 'white-text' : 'red-text')} href="#">
-                    Sass
-                </a>
-            </li>
-            <li>
-                <a className={' ' + (props.theme === 'dark' ? 'white-text' : 'red-text')} href="#">
-                    Components
-                </a>
-            </li>
-            <li>
-                <a className={' ' + (props.theme === 'dark' ? 'white-text' : 'red-text')} href="#">
-                    Javascript
-                </a>
-            </li>
-            <li>
-                <a className={' ' + (props.theme === 'dark' ? 'white-text' : 'red-text')} href="#">
-                    Mobile
-                </a>
-            </li>
-        </ul>
+        <ThemeContext.Consumer>
+            {theme => (
+                <ul className="right hide-on-med-and-down">
+                    {console.log(theme)}
+                    <li>
+                        <a
+                            theme={theme}
+                            className={' ' + (theme === 'dark' ? 'white-text' : 'red-text')}
+                            href="#"
+                        >
+                            Sass
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            theme={theme}
+                            className={' ' + (theme === 'dark' ? 'white-text' : 'red-text')}
+                            href="#"
+                        >
+                            Components
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            theme={theme}
+                            className={' ' + (theme === 'dark' ? 'white-text' : 'red-text')}
+                            href="#"
+                        >
+                            Javascript
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            theme={theme}
+                            className={' ' + (theme === 'dark' ? 'white-text' : 'red-text')}
+                            href="#"
+                        >
+                            Mobile
+                        </a>
+                    </li>
+                </ul>
+            )}
+        </ThemeContext.Consumer>
     );
 }
---
+++ src/MyProvider.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+class MyProvider extends Component {
+  state = {
+
+  }
+  render() {
+    return(
+      
+    )
+  }
+}
+
+default export MyProvider
